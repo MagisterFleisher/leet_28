914ee99242d43c08fd0cc8bdbdc62647e8694326
Aaron Fleisher <amfleish@gmail.com>
2024-09-22 13:22:21 -0700

    messing with string length

--- src/include/string_check.c
+++ src/include/string_check.c
@@ -8,10 +8,36 @@ void temp(void) {
 }
 
 size_t stringLength(char* current_string, bool with_nullchar) {
-  if(with_nullchar) { return sizeof(current_string); }
-  return (sizeof(current_string) - 1); }
+  size_t len = 0;
+  while('\0' != current_string[len]) { len++; }
+  if(with_nullchar) { return len + 1; }
+  return len;
+}
+/* 
+size_t stringLength2(char* current_string) {
+  size_t len = 0;
+  while(0 ^ current_string[len]) { 
+    len++; }
+  return len;
+  }
+ */
+
+size_t stringLength2(char* current_string) {
+  //size_t len = 0;
+  char* b = current_string;
+  while(0 ^ *b) { 
+    b++; }
+  //(void) printf("%zu\n", (b - current_string));
+  return (b - current_string);
+}
 
 int strStr(char* haystack, char* needle) {
+    enum switch_cases {
+      WRONG_SIZE = 1,
+      SAME_SIZE_NOT_1 = 2,
+      NEEDLE_1 = 3,
+      BOTH_1 = 4
+    };
     int first_occurence = -1;
     /* 
     Trivial Cases: 
@@ -33,6 +59,83 @@ int strStr(char* haystack, char* needle) {
     Parallizable:
       Yes.  If big enough, start seaching by chunking up the search space.
     */
+    uint16_t haystack_length = (uint16_t) stringLength(haystack, false);
+    uint16_t needle_length = (uint16_t) stringLength(needle, false);
+    bool haystack_length_0 = (0 == haystack_length);
+    bool needle_length_0 = (0 == needle_length);
+    bool needle_length_1 = (1 == needle_length);
+    bool haystack_length_1 = (1 == haystack_length);
+    bool haystack_less_needle = (haystack_length < needle_length);
+    bool haystack_same_needle = (haystack_length == needle_length);
+
+    uint16_t is_wrong_size = (haystack_length_0 || needle_length_0 || haystack_less_needle);
+    uint16_t is_same_size_not_1 = 2 * (haystack_same_needle & !needle_length_1);
+    uint16_t is_needle_1 = 3 * (needle_length_1 && !haystack_less_needle && !haystack_same_needle);
+    uint16_t is_both_1 = 4 * (needle_length_1 && haystack_length_1);
+    uint16_t case_value = is_wrong_size + is_same_size_not_1 + is_needle_1 + is_both_1;
+/* 
+    (void) printf("haystack: %s\tneedle:%s\n", haystack, needle);
+    (void) printf("haystack_length: %d\tneedle_length:%d\n", haystack_length, needle_length);
+
+    (void) printf("haystack_length_0: %d\n", haystack_length_0);
+    (void) printf("needle_length_0: %d\n", needle_length_0);
+
+    (void) printf("haystack_length_1: %d\n", haystack_length_1);
+    (void) printf("needle_length_1: %d\n", needle_length_1);
     
+    (void) printf("haystack_length_bigger: %d\n", haystack_length_bigger);
+    (void) printf("needle_length_bigger: %d\n", needle_length_bigger);
+    
+    (void) printf("haystack_less_needle: %d\n", haystack_less_needle);
+    (void) printf("haystack_same_needle: %d\n", haystack_same_needle);
+    (void) printf("\n\n");
+ */
+/*     uint16_t case_value = case_value1 + case_value2 + case_value3 + case_value4; */
+
+    switch(case_value) {
+      case WRONG_SIZE : {
+        (void) printf("%s %d- WRONG SIZE\thaystack: %s\tneedle:%s\n",__func__, __LINE__, haystack, needle);
+        break;
+      };
+      case BOTH_1 : {
+        if(haystack[0] == needle[0]) { first_occurence = 0; }
+        (void) printf("%s %d- SAME SIZE\thaystack: %s\tneedle:%s\n", __func__, __LINE__, haystack, needle);
+        break;
+      };
+      case SAME_SIZE_NOT_1 : {
+        char* tmp_haystack_0 = haystack;
+        char* tmp_needle_0 = needle; 
+        uint16_t tmp_same = 0;
+        while('\0' != *tmp_haystack_0) {
+          if(*tmp_haystack_0 != *tmp_needle_0) {
+            tmp_same++;} 
+          tmp_haystack_0++;
+          tmp_needle_0++;
+        }
+        if(0 == tmp_same) { first_occurence = 0; }
+        (void) printf("%s %d- SAME SIZE\thaystack: %s\tneedle:%s\n", __func__, __LINE__, haystack, needle);
+        break;
+      };
+      case NEEDLE_1  : {
+        (void) printf("%s %d- NEEDLE 1\thaystack: %s\tneedle:%s\n", __func__, __LINE__, haystack, needle);
+        char* tmp_haystack_1 = haystack;
+        uint16_t tmp_index = 0;
+        while('\0' == *tmp_haystack_1) {
+          if(*tmp_haystack_1 == needle[0]) {
+            first_occurence = (int) tmp_index;
+          } else {
+            tmp_haystack_1++;
+            tmp_index++;
+          }
+        }
+        break;
+      };
+      default: {
+        (void) printf("%s %d- default\thaystack: %s\tneedle:%s\n", __func__, __LINE__, haystack, needle);
+        first_occurence = -2;
+        break;
+      }
+    }
+
     return first_occurence;
 }
--- src/main.c
+++ src/main.c
@@ -1,14 +1,31 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include "include/string_check.h"
-#define BIGNUM 100000000
+#define BIGNUM 10000000
 
  void profileMyStringFunc(char* current_string) {
-  uint16_t my_string_length = 0;
-  for(uint32_t index = 0; index < BIGNUM; index++) {
+  size_t my_string_length = 0;
+  //uint16_t uint_my_string_length = 0;
+  for(uint32_t index = 0; index < 10000; index++) {
     my_string_length = stringLength(current_string, false);
   }
-//  (void) printf("1 my_string_length: %d\n", my_string_length);
+/*   uint_my_string_length = (uint16_t) my_string_length;
+  (void) printf("1 my_string_length: %zu\tsize of size_t: %zu\n", my_string_length, sizeof(my_string_length));
+  (void) printf("1 my_string_length: %d\tsize of uint16_t: %zu\n", uint_my_string_length, sizeof(uint_my_string_length)); */
+  return;
+}
+
+
+ void profileMyStringFunc2(char* current_string) {
+  size_t my_string_length = 0;
+  //uint16_t uint_my_string_length = 0;
+  for(uint32_t index = 0; index < BIGNUM; index++) {
+    my_string_length = stringLength2(current_string);//, false);
+  }
+  // uint_my_string_length = (uint16_t) my_string_length;
+  //(void) printf("1 my_string_length: %zu\tsize of size_t: %zu\n", my_string_length, sizeof(my_string_length));
+//  (void) printf("1 my_string_length: %d\tsize of uint16_t: %zu\n", uint_my_string_length, sizeof(uint_my_string_length));
   return;
 }
 
@@ -17,24 +34,65 @@ void profileStdStringFunc(char* current_string) {
   for(uint32_t index = 0; index < BIGNUM; index++) {
     my_string_length = strlen(current_string);
   }
-  //(void) printf("2 my_string_length: %zu\n", my_string_length);
+  // (void) printf("2 my_string_length: %zu\n", my_string_length);
   return;
 }
 
+void profstr(char* current_string) {
+ size_t my_string_length = 0;
+for(uint32_t index = 0; index < BIGNUM; index++) {
+    my_string_length = stringLength2(current_string);//, false);
+  }
+  return;
+}
 int main(int argument_count, char** argument_vector) {
   int error = 0;
-  // (void) printf("%s] %s) %d- argument_count: %d\n", __FILE__, __FUNCTION__, __LINE__, argument_count);
-  char* haystack = "sadbutsad";
-  char* needle = "sad";
 
-  /*
+  char* current_string = "hello";
+
+  //profileMyStringFunc2(current_string);
+  //profileMyStringFunc(current_string);
+  profileStdStringFunc(current_string);
+  profstr(current_string);
+  // (void) printf("%s] %s) %d- argument_count: %d\n", __FILE__, __FUNCTION__, __LINE__, argument_count);
+/* 
+  char* haystack0 = "sadbutsad";
+  char* needle0 = "sad";
+
+  char* haystack1 = "s";
+  char* needle1 = "sad";
+
+  char* haystack2 = "s";
+  char* needle2 = "s";
+
+  char* haystack3 = "s";
+  char* needle3 = "d";
+
+  char* haystack4 = "s";
+  char* needle4 = "sd";
+
+  char* haystack5 = "sh";
+  char* needle5 = "si";
+
+  char* haystack6 = "sit";
+  char* needle6 = "sit";
+
+  
   char* haystack = "leetcode";
   char* needle = "leeto";
-  */
 
-  //int first_occurence = strStr(haystack, needle);
-  (void) profileMyStringFunc(haystack);
-  (void) profileStdStringFunc(haystack);
-  
+
+  // int first_occurence = strStr(haystack, needle);
+  //(void) profileMyStringFunc(haystack);
+  //(void) profileStdStringFunc(haystack);
+
+  (void) printf("case 0 first occurence: %d\n", strStr(haystack0, needle0));
+  (void) printf("case 1 first occurence: %d\n", strStr(haystack1, needle1));
+  (void) printf("case 2 first occurence: %d\n", strStr(haystack2, needle2));
+  (void) printf("case 3 first occurence: %d\n", strStr(haystack3, needle3));
+  (void) printf("case 4 first occurence: %d\n", strStr(haystack4, needle4));
+  (void) printf("case 5 first occurence: %d\n", strStr(haystack5, needle5));
+  (void) printf("case 6 first occurence: %d\n", strStr(haystack6, needle6));
+ */
   return error;
 }
